018110f7e58d8a12db2e4079f8c53d94cb48ec91
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-11-08 23:41:32 +0300

    refactored game loop

--- core/game_loop.py
+++ core/game_loop.py
@@ -1,25 +1,27 @@
-import random
-from core.dungeon import generate_dungeon
-from core.events import (
-    EventLoop, shoot_arrow,
-    check_room_events, check_close_events
-)
-from core.objects import player
-
-ATACK_ACTION = 1
-MOVE_ACTION = 2
+from core.actions import ATACK, MOVE, WAIT
+from core.coroutines import shoot_arrow
+from core.dungeon import generate_dungeon, generate_events_and_beasts
+from core.events import check_room_events, check_close_events
+from core.event_loop import event_loop
+from core.objects import Player, beast_map
+from config import WUMPUSES
 
 
 def start_game_loop():
     """
+    - Removes garbage
     - Generates events in dungeon
     - Revives the player
     - Starts the game loop
     """
+    # Removes garbage
+    event_loop.clear()
+    beast_map.clear()
+    # generate events in rooms
     rooms = generate_dungeon()
-    player.is_dead = False
+    generate_events_and_beasts(rooms)
+    player = Player()
     current_room = rooms['1']
-    event_loop = EventLoop()
     while True:
         # current_room = player.current_room
         print(f'\nYou are in the room {current_room.num}')
@@ -31,21 +33,29 @@ def start_game_loop():
         # game over
         if player.is_dead:
             return
+        # WIN
+        if player.kills == WUMPUSES:
+            text = "You won! You found {} gold and killed {} monster(s) known as Wumpus"
+            print(text.format(player.gold, player.kills))
+            return
         # check close events
         check_close_events(current_room)
         # player is still alive let's do something
-        options = set(room.num for room in current_room.connected_rooms.values())
+        options = set(
+            room.num for room in current_room.connected_rooms.values()
+        )
         print(f'Tunnels lead to {" ".join(options)} ')
         # player's action
         print(f'You have {player.arrows} arrow(s).')
         action = ask_to_act(player)
-        if action is ATACK_ACTION:
+        if action is ATACK:
             player.arrows -= 1
             shot_power = ask_shot_power()
             flying_curve = ask_flying_curve(shot_power)
             event_loop.add_event(shoot_arrow(current_room, flying_curve))
-        elif action is MOVE_ACTION:
-            current_room = rooms[ask_player_to_move_from(current_room, options)]
+        elif action is MOVE:
+            next_room_num = ask_player_to_move_from(current_room, options)
+            current_room = rooms[next_room_num]
 
         # end player's turn
         event_loop.update_events()
@@ -67,6 +77,7 @@ def ask_shot_power():
             continue
         return power
 
+
 def ask_player_to_move_from(room, options):
     while True:
         response = input("Where to? ")
@@ -80,14 +91,14 @@ def ask_player_to_move_from(room, options):
 def ask_to_act(player):
     """Asks about next player's action"""
     while True:
-        response = input('Shoot or Move? [S/M] ').lower()
+        response = input('Shoot or Move? [s/m/w] ').lower()
         if response == 's':
             if player.arrows < 1:
                 print("There are no more arrows left... You must move on.")
-                return MOVE_ACTION
-            return ATACK_ACTION
+                return MOVE
+            return ATACK
 
         if response == 'm':
-            return MOVE_ACTION
+            return MOVE
         if response == 'w':
-            return 3
+            return WAIT
