d875e02d7b83368f8c26f8a1cb79edcd12c0c60b
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-11-09 21:12:12 +0300

    refactored main

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from core.dungeon import generate_dungeon
-from core.objects import player
-from core.events import GAME_OVER
-from text_lines import events_text, intro
-
-
-def start_game_loop():
-    rooms = generate_dungeon()
-    current_room = rooms[1]
-    while True:
-        if player.dead:
-            print(events_text[GAME_OVER])
-            return
-        print(f'\nYou are in the room {current_room.num}')
-        for event in current_room.events:
-            print(events_text[event])
-        action = ask_player_to_act()
-        if action is 1:
-            pass
-        if action is 2:
-            current_room = rooms[ask_player_to_move_from(current_room)]
+from text_lines import intro
+from core.game_loop import start_game_loop
 
 
 def start_game():
@@ -27,26 +8,6 @@ def start_game():
         start_game_loop()
         if (answer := ask_player_to_restart()) is False:
             break
-
-
-def ask_player_to_move_from(room):
-    options = set(str(room.num) for room in room.connected_rooms.values())
-    while True:
-        print(f'\nYou can go to the following rooms: {" ".join(options)} ')
-        response = input("Where to? ")
-
-        if response not in options:
-            continue
-        return int(response)
-
-
-def ask_player_to_act():
-    while True:
-        response = input('Shoot or Move? [S/M] ').lower()
-        if response == 's':
-            return 1
-        if response == 'm':
-            return 2
 
 
 def ask_player_to_restart():
