5315be8e6a9605420608538a0d8b18b646417d48
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-11-11 01:18:49 +0300

    updated game objects

--- core/objects.py
+++ core/objects.py
@@ -2,21 +2,41 @@ from config import ARROWS
 
 
 class Room:
+    __slots__ = ('num', 'events', 'connected_rooms')
+
     def __init__(self, num):
-        self.num = num
+        self.num = str(num)
         self.connected_rooms = {}
-        self.events = []
+        self.events = set()
 
-
-class Arrow:
-    def __init__(self, pos, flying_route):
-        self.pos = pos
+    def __repr__(self):
+        return self.num
 
 
 class Player:
+    __slots__ = ('arrows', 'is_dead', 'gold', 'kills')
+
     def __init__(self):
         self.arrows = ARROWS
-        self.dead = False
+        self.is_dead = False
+        self.gold = 0
+        self.kills = 0
 
 
-player = Player()
+class BeastMap:
+    __slots__ = ('beasts',)
+
+    def __init__(self):
+        self.beasts = {}
+
+    def __getitem__(self, v):
+        return self.beasts[v]
+
+    def __setitem__(self, k, v):
+        self.beasts[k] = v
+
+    def clear(self):
+        self.beasts.clear()
+
+
+beast_map = BeastMap()
